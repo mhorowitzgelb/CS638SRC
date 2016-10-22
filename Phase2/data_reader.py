291ed09ea5213502da52953a1e7ed61549a17803
mhorowitzgelb <mhorowitzgelb@hotmail.com>
2016-10-22 14:54:54 -0500

    Finished datareader code

--- Phase2/data_reader.py
+++ Phase2/data_reader.py
@@ -1,4 +1,5 @@
-
+import sys
+from matplotlib import pyplot
 '''
 Pass a file path and this simple method will create a dictionary of lists for each column
 in the dataset
@@ -25,3 +26,63 @@ def readData(file):
         data['product_id'].append(split[7])
         data['url'].append(split[8])
     return data
+'''
+Returns a tuple of (missing,total)
+for specified data and column name
+'''
+def missingValues(data, column_name):
+    missing = 0
+    total = 0
+    if column_name in data:
+        list = data[column_name]
+        total = len(list)
+        for val in list:
+            if val is '':
+                missing +=1
+
+    else:
+        return (-1,-1)
+    return (missing, total)
+
+
+'''
+Returns a tuple of the (min, average, max) string lengths of the
+specified datamatrix and column. (Note strings of length 0 are not
+considered)
+'''
+def stringLengthSummary(data, column_name):
+    total = 0
+    minlen = sys.maxint
+    maxlen = -sys.maxint - 1
+    notMissing = 0
+    if column_name in data:
+        for str in data[column_name]:
+            if not str is '':
+                notMissing += 1
+                minlen = min(minlen,len(str))
+                maxlen = max(maxlen, len(str))
+                total += len(str)
+        return (minlen, total / notMissing, maxlen)
+    else:
+        return (-1,-1,-1)
+
+def stringLengthHistogram(data, column_name, num_bins):
+    lengths = []
+    if column_name in data:
+        for str in data[column_name]:
+            if not str is '':
+                lengths.append(len(str))
+        pyplot.hist(lengths,label=column_name,bins=num_bins)
+        pyplot.show()
+
+def missingComparison(data, missingCol, compareCol):
+    compare = []
+
+    if missingCol in data and compareCol in data:
+        zipped =  zip(data[missingCol], data[compareCol])
+        for pair in zipped:
+            if pair[0] is '':
+                compare.append(pair[1])
+    return compare
+
+
